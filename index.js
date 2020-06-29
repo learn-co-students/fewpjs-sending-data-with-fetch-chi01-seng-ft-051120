edc56561fe796f553257030c6369b8d9d42b5da7
emilioquintana90 <emilioquintana90@gmail.com>
2020-06-28 19:54:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+const body = document.querySelector("body");
+
+function submitData(name, email) {
+  let formData = {
+    name: name,
+    email: email,
+  };
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then((resp) => resp.json())
+    .then((object) => appendId(object))
+    .catch((error) => appendError(error));
+}
+
+function appendId(object) {
+  body.innerHTML += `
+    <p>${object.id}</p>
+  `;
+}
+
+function appendError(error) {
+  body.innerHTML += `
+    <p>${error.message}</p>
+  `;
+}
