4525fc58975c8491b0a520e33e188fb3aa40ae3c
okaydokay97 <rubens.fernandes97@gmail.com>
2020-06-29 09:56:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+const body = document.querySelector("body")
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+  .then(resp => resp.json())
+  .then(userData => {
+    // const pTag = document.createElement("p")
+    // pTag.innerHTML = userData.id
+    // body.appendChild(pTag)
+    const pTag = `<p>${userData.id}</p>`
+    body.innerHTML = pTag
+    })
+  .catch((error) => {
+    const errorTag = `<p>${error}</p>` 
+    body.innerHTML = errorTag
+  })
+  
+  }
